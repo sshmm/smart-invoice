546339f632e25e2de30783a330a6f25ae78d7336
sshmm <s.shawky92@gmail.com>
2024-11-27 22:19:15 +0200

    update useEscrowZap inputs

--- packages/hooks/src/types.ts
+++ packages/hooks/src/types.ts
@@ -1,7 +1,17 @@
-import {
-  useSimulateContract,
-  useWriteContract,
-} from 'wagmi';
+import { useSimulateContract, useWriteContract } from 'wagmi';
 
-export type SimulateContractErrorType = ReturnType<typeof useSimulateContract>["error"];
-export type WriteContractErrorType = ReturnType<typeof useWriteContract>["error"];
+export type SimulateContractErrorType = ReturnType<
+  typeof useSimulateContract
+>['error'];
+
+export type WriteContractErrorType = ReturnType<
+  typeof useWriteContract
+>['error'];
+
+export type ProjectDetails = {
+  projectName: string;
+  projectDescription: string;
+  projectAgreement: string;
+  startDate: number;
+  endDate: number;
+};
